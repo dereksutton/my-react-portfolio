388570eeb268552bd961d7edd1ed2d058167d2c3
Derek Sutton <dereksutton86@gmail.com>
2023-06-13 12:48:54 -0400

    added basic structure and styling for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header></header>
-    )
+        <header className='text-center py-5 bg-blue-500 text-white'>
+            <h1 className='text-4x1'>Derek Sutton</h1>
+        </header>
+    );
 }
+
+export default Header;
