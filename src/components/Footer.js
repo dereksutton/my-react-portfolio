3abab09c1774e7b340734e65985b068e6f7d254b
Derek Sutton <dereksutton86@gmail.com>
2023-06-13 12:49:36 -0400

    added basic structure and styling for Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className='py-5 bg-blue-500 text-center text-white'>
+            <p>Copyright &copy; 2023 Derek Sutton</p>
+        </footer>
+    );
+}
+
+export default Footer;
