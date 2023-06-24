c413930e282efa3c4f870dbef2ae4fe62784aaed
Derek Sutton <dereksutton86@gmail.com>
2023-06-24 17:51:30 -0400

    reconfigured Portfolio component to pass in the Project component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,45 +1,13 @@
 import React from 'react';
+import Project from './Project';
 
-const Portfolio = () => {
-    return (
+const Portfolio = ({ projects }) => {
+        return (
         <section>
             <h2>Portfolio</h2>
-            <div>
-                <h3>Project 1</h3>
-                <img src="#" alt="Project 1 Screenshot"/>
-                <a href="#">Deployed Application</a>
-                <a href="#">GitHub Repository</a>
-            </div>
-            <div>
-                <h3>Project 2</h3>
-                <img src="#" alt="Project 2 Screenshot"/>
-                <a href="#">Deployed Application</a>
-                <a href="#">GitHub Repository</a>
-            </div>
-            <div>
-                <h3>Project 3</h3>
-                <img src="#" alt="Project 3 Screenshot"/>
-                <a href="#">Deployed Application</a>
-                <a href="#">GitHub Repository</a>
-            </div>
-            <div>
-                <h3>Project 4</h3>
-                <img src="#" alt="Project 4 Screenshot"/>
-                <a href="#">Deployed Application</a>
-                <a href="#">GitHub Repository</a>
-            </div>
-            <div>
-                <h3>Project 5</h3>
-                <img src="#" alt="Project 5 Screenshot"/>
-                <a href="#">Deployed Application</a>
-                <a href="#">GitHub Repository</a>
-            </div>
-            <div>
-                <h3>Project 6</h3>
-                <img src="#" alt="Project 6 Screenshot"/>
-                <a href="#">Deployed Application</a>
-                <a href="#">GitHub Repository</a>
-            </div>
+            {projects.map((project, index) => (
+                <Project key={index} project={project} />
+            ))}
         </section>
     );
 }
