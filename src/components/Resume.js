9b4955df23b04038822963920b1511975999070c
Derek Sutton <dereksutton86@gmail.com>
2023-06-27 10:54:46 -0400

    refined styles for Resume component, made layout responsive

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -31,21 +31,21 @@ const Resume = () => {
     return (
         <section id='resume' className="flex flex-col items-center justify-center py-4 bg-gray-100">
             <h2 className='text-2xl font-bold mb-8'>Proficiencies</h2>
-            <div className='flex flex-wrap justify-center w-full md:w-4/5 lg:w-2/3'>
-                {skills.map((skill, index) => (
-                    <div key={index} className='m-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4 bg-white shadow-lg rounded-md text-center transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
-                        <img src={skill.logo} alt={skill.name} className='h-16 w-16 mx-auto mb-2' />
-                        <p>{skill.name}</p>
-                    </div>
-                ))}
-            </div>
-            <div className="mt-5">
+            <div className="mt-8 mb-8">
                 <a href="/DerekSutton-Resume2023.pdf" download className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Download Resume
                 </a>
             </div>
+            <div className='mb-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full md:w-4/5 lg:w-2/3'>
+                {skills.map((skill, index) => (
+                    <div key={index} className='p-4 bg-white shadow-lg rounded-md text-center transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
+                        <img src={skill.logo} alt={skill.name} className='h-24 w-24 mx-auto mb-2' />
+                        <p>{skill.name}</p>
+                    </div>
+                ))}
+            </div>
         </section>
-    )
+    );
 }
 
 export default Resume;
